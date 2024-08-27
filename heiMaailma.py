5b68e15bf9f52da3c646cd9e2d3dadf859ec45b6
Elias <elias.granlund@metropolia.fi>
2024-08-27 19:30:12 +0300

    kotitehtävät

--- heiMaailma.py
+++ heiMaailma.py
@@ -1,1 +1,12 @@
 print ("Hei, maailma!")
+käyttäjä = input ("Anna nimesi:")
+print("Hauska tavata, " + käyttäjä +"!")
+# Luetaan syötteet
+kanta=float(input("Anna kolmion kanta: "))
+korkeus=float(input("Anna kolmion korkeus: "))
+# Lasketaan ala syötteiden perusteella
+ala = (kanta * korkeus) / 2
+# Tulostetaan vastaus
+#print("Kolmion ala on: "+ str(ala))
+print(f"Kolmion ala on{ala:10.2f}")
+
